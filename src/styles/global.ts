ab645bd0d67b433c93348e1f292290e900a48d8c
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-07-13 19:24:04 -0300

    style: add global style

---
+++ src/styles/global.ts
@@ -0,0 +1,87 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+    background-color: #D9E6F6;
+  }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  :root {
+    --backgroundPrimary: #D9E6F6;
+    --backgroundSecondary: #F1F9FE;
+    --backgroundTertiary: #FFFFFF;
+    --backgroundQuarternary: #BBCDE8;
+    --colorPrimary: #2E7BB4;
+    --colorSecondary: #388BB0;
+    --colorTertiary: #2F4A71;
+    --colorQuarternary: #D81D99;
+    --textPrimaryColor: #333333;
+    --textSecondaryColor: #FFFFFF;
+    --textTertiaryColor: #5A5A5A;
+    --textQuarternaryColor: #C5C6CA;
+    --commonRadius: 8px;
+  }
+
+  *::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  *::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 10px;
+  }
+
+  *::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+
+  a,
+  button {
+    cursor: pointer;
+    transition: .3s;
+    outline: 0;
+    &:hover,
+    &:focus {
+      opacity: .8;
+    }
+    &:disabled {
+      cursor: not-allowed;
+      opacity: .5;
+    }
+  }
+
+  input {
+    transition: .3s;
+    outline: 0;
+    &:disabled {
+      cursor: not-allowed;
+      opacity: .5;
+    }
+    &:hover,
+    &:focus {
+      box-shadow: 0px 0px 5px #33333357;
+    }
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+`;
