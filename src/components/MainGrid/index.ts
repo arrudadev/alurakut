196c7dab407a0c2731e0e826466dd1a6e79b0ed3
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-07-14 00:31:12 -0300

    chore: add MainGrid component

---
+++ src/components/MainGrid/index.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const MainGrid = styled.main`
+  width: 100%;
+  max-width: 500px;
+
+  padding: 16px;
+  margin-left: auto;
+  margin-right: auto;
+
+  .profile-area {
+    display: none;
+    @media (min-width: 860px) {
+      display: block;
+    }
+  }
+
+  @media (min-width: 860px) {
+    max-width: 1110px;
+
+    display: grid;
+    grid-gap: 10px;
+    grid-template-areas: 'profile-area welcome-area profile-relation-area';
+    grid-template-columns: 160px 1fr 312px;
+  }
+`;
