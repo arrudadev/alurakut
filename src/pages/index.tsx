aaae0fb78e78060c68b11bd3478493832d9d06cb
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-07-15 22:11:15 -0300

    chore: add Home page structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,60 @@
+import { Box } from '../components/Box';
+import { IconSet } from '../components/IconSet';
+import { MainGrid } from '../components/MainGrid';
 import { Menu } from '../components/Menu';
+import { ProfileRelationsBoxWrapper } from '../components/ProfileRelationsBoxWrapper';
+import { ProfileSidebar } from '../components/ProfileSidebar';
 
 export default function Home() {
-  return <Menu githubUser="monteiro-alexandre" />;
+  const users = [
+    'juunegreiros',
+    'omariosouto',
+    'peas',
+    'rafaballerini',
+    'marcobrunodev',
+    'felipefialho',
+  ];
+
+  return (
+    <>
+      <Menu githubUser="monteiro-alexandre" />
+      <MainGrid>
+        <div className="profile-area" style={{ gridArea: 'profile-area' }}>
+          <ProfileSidebar githubUser="monteiro-alexandre" />
+        </div>
+
+        <div className="welcome-area" style={{ gridArea: 'welcome-area' }}>
+          <Box>
+            <h1 className="title">Bem vindo(a)</h1>
+
+            <IconSet />
+          </Box>
+        </div>
+
+        <div
+          className="profile-relations-area"
+          style={{ gridArea: 'profile-relation-area' }}
+        >
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({users.length})
+            </h2>
+
+            <ul>
+              {users.map(user => {
+                return (
+                  <li key={user}>
+                    <a href={`/users/${user}`}>
+                      <img src={`https://github.com/${user}.png`} alt={user} />
+                      <span>{user}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  );
 }
