2d0d6bcbd59f2923bdfcc33e5e83d0e7ab059a5d
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-07-15 23:03:33 -0300

    chore: add community creation form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { FormEvent, useState } from 'react';
+
 import { Box } from '../components/Box';
 import { IconSet } from '../components/IconSet';
 import { MainGrid } from '../components/MainGrid';
@@ -6,6 +8,17 @@ import { ProfileRelationsBoxWrapper } from '../components/ProfileRelationsBoxWra
 import { ProfileSidebar } from '../components/ProfileSidebar';
 
 export default function Home() {
+  const [communities, setCommunities] = useState([
+    {
+      id: '12802378123789378912789789123896123',
+      title: 'Eu odeio acordar cedo',
+      image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg',
+    },
+  ]);
+
+  const [communityTitle, setCommunityTitle] = useState('');
+  const [communityImage, setCommunityImage] = useState('');
+
   const users = [
     'juunegreiros',
     'omariosouto',
@@ -14,6 +27,23 @@ export default function Home() {
     'marcobrunodev',
     'felipefialho',
   ];
+
+  function handleCreateCommunity(event: FormEvent) {
+    event.preventDefault();
+
+    if (communityTitle.length > 0 && communityImage.length > 0) {
+      const community = {
+        id: new Date().toISOString(),
+        title: communityTitle,
+        image: communityImage,
+      };
+
+      setCommunities([...communities, community]);
+
+      setCommunityTitle('');
+      setCommunityImage('');
+    }
+  }
 
   return (
     <>
@@ -28,6 +58,33 @@ export default function Home() {
             <h1 className="title">Bem vindo(a)</h1>
 
             <IconSet />
+          </Box>
+
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form onSubmit={handleCreateCommunity}>
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                  value={communityTitle}
+                  onChange={event => setCommunityTitle(event.target.value)}
+                />
+              </div>
+              <div>
+                <input
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                  value={communityImage}
+                  onChange={event => setCommunityImage(event.target.value)}
+                />
+              </div>
+
+              <button type="submit">Criar comunidade</button>
+            </form>
           </Box>
         </div>
 
