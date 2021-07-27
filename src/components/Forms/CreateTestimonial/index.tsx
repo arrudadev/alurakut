51fdecc99603a82c3f3ea1194aef690c73b2a659
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-07-26 21:23:42 -0300

    chore: add CreateTestimonialForm component

---
+++ src/components/Forms/CreateTestimonial/index.tsx
@@ -0,0 +1,62 @@
+import { FormEvent, useState } from 'react';
+
+type Testimonial = {
+  githubUser: string;
+  testimonial: string;
+};
+
+type CreateTestimonialFormProps = {
+  testimonials: Testimonial[];
+  setTestimonials(testimonials: Testimonial[]): void;
+};
+
+export function CreateTestimonialForm({
+  testimonials,
+  setTestimonials,
+}: CreateTestimonialFormProps) {
+  const [githubUser, setGithubUser] = useState('');
+  const [testimonial, setTestimonial] = useState('');
+
+  function handleCreateTestimonial(event: FormEvent) {
+    event.preventDefault();
+
+    if (githubUser.length > 0 && testimonial.length > 0) {
+      const newTestimonial = {
+        id: new Date().toISOString(),
+        githubUser,
+        testimonial,
+      };
+
+      setTestimonials([...testimonials, newTestimonial]);
+
+      setGithubUser('');
+      setTestimonial('');
+    }
+  }
+
+  return (
+    <form onSubmit={handleCreateTestimonial}>
+      <div>
+        <input
+          placeholder="Digite seu usuário do github?"
+          name="user"
+          aria-label="Digite seu usuário do github?"
+          type="text"
+          value={githubUser}
+          onChange={event => setGithubUser(event.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          placeholder="Digite seu depoimento..."
+          name="testimonial"
+          aria-label="Digite seu depoimento..."
+          value={testimonial}
+          onChange={event => setTestimonial(event.target.value)}
+        />
+      </div>
+
+      <button type="submit">Criar depoimento</button>
+    </form>
+  );
+}
